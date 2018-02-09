65a11007328d7af26a03ed6b755c7b18d75cb548
IcheNiche <icey@icey.de>
2018-02-09 22:13:58 +0100

    Door interaction

---
+++ Assets/Scripts/Interaction/RotatorGrabAttach.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK.GrabAttachMechanics;
+
+public class RotatorGrabAttach : VRTK_RotatorTrackGrabAttach
+{
+
+    public Rigidbody grabbedRigidBody;
+
+    public override bool StartGrab(GameObject grabbingObject, GameObject givenGrabbedObject, Rigidbody givenControllerAttachPoint)
+    {
+        bool ret = base.StartGrab(grabbingObject, givenGrabbedObject, givenControllerAttachPoint);
+
+        grabbedObjectRigidBody = grabbedRigidBody;
+
+        return ret;
+    }
+
+}
