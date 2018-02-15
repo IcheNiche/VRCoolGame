2670bbef7e60b35d4131f10f7008141bf81986fe
IcheNiche <icey@icey.de>
2018-02-16 00:09:03 +0100

    UPDATE: Send object grab release impuls

--- Assets/Scripts/Network/NetVRTK/NetworkGrabManager.cs
+++ Assets/Scripts/Network/NetVRTK/NetworkGrabManager.cs
@@ -9,13 +9,16 @@
 
     [RequireComponent(typeof(VRTK_InteractableObject))]
     public class NetworkGrabManager : NetworkBehaviour {
-        public PhotonView[] ownAdditionalPhotonviews;
 
+        public Rigidbody rigidBody;
+
+        public PhotonView[] ownAdditionalPhotonviews;
+        
         private int grabOwner;
 
-        private VRTK_InteractableObject io;
-        private NetworkReference nref;
-
+        private VRTK_InteractableObject interactableObject;
+        private NetworkReference networkReference;
+        
         private bool wasKinimatic = false;
 
         public int currentGrabOwner {
@@ -25,55 +28,103 @@
         }
 
         void Awake() {
-            io = GetComponent<VRTK_InteractableObject>();
-            nref = NetworkReference.FromObject(this.gameObject);
-            propKey = PROP_KEY_ID + nref.parentHandleId + "$" + (nref.pathFromParent != null ? nref.pathFromParent : "") + "$";
+            interactableObject = GetComponent<VRTK_InteractableObject>();
+            networkReference = NetworkReference.FromObject(this.gameObject);
+            propKey = PROP_KEY_ID + networkReference.parentHandleId + "$" + (networkReference.pathFromParent != null ? networkReference.pathFromParent : "") + "$";
             var dummy = PropertyEventHandler.Instance;
         }
 
         void OnEnable() {
-            io.InteractableObjectGrabbed += HandleGrab;
-            io.InteractableObjectUngrabbed += HandleUngrab;
-            if (nref.IsPhotonView) {
-                InitState(nref.GetPhotonView().ownerId);
+            interactableObject.InteractableObjectGrabbed += HandleGrab;
+            interactableObject.InteractableObjectUngrabbed += HandleUngrab;
+            if (networkReference.IsPhotonView) {
+                InitState(networkReference.GetPhotonView().ownerId);
             }
+
+            PhotonNetwork.OnEventCall += this.OnUpdateClientObjectRoomData;
         }
 
         void OnDisable() {
-            io.InteractableObjectGrabbed -= HandleGrab;
-            io.InteractableObjectUngrabbed -= HandleUngrab;
+            interactableObject.InteractableObjectGrabbed -= HandleGrab;
+            interactableObject.InteractableObjectUngrabbed -= HandleUngrab;
+
+            PhotonNetwork.OnEventCall -= this.OnUpdateClientObjectRoomData;
         }
 
+
+
         private void HandleGrab(object sender, InteractableObjectEventArgs e) {
-            if (nref.IsPhotonView) {
-                nref.GetPhotonView().TransferOwnership(PhotonNetwork.player);
+            if (networkReference.IsPhotonView) {
+                networkReference.GetPhotonView().TransferOwnership(PhotonNetwork.player);
             }
             foreach (PhotonView pv in ownAdditionalPhotonviews) {
                 pv.TransferOwnership(PhotonNetwork.player);
             }
             InitState(PhotonNetwork.player.ID);
             SendState();
+
+            UpdateObjectRoomDataToAllClients(true);
         }
 
         private void HandleUngrab(object sender, InteractableObjectEventArgs e) {
             InitState(0);
             SendState();
+
+            UpdateObjectRoomDataToAllClients(false);
+        }
+
+        void UpdateObjectRoomDataToAllClients(bool grabbed)
+        {
+            Hashtable content = new Hashtable();
+
+            content.Add("grabbed", grabbed);
+
+            content.Add("position", transform.position);
+            content.Add("rotation", transform.rotation);
+
+            if (rigidBody != null)
+            {
+                content.Add("velocity", rigidBody.velocity);
+                content.Add("angularVelocity", rigidBody.angularVelocity);
+            }
+
+            PhotonNetwork.RaiseEvent((byte)RaiseEventCodes.UpdateObjectRoomData, content, true, null);
+        }
+
+
+        void OnUpdateClientObjectRoomData(byte eventCode, object eventContent, int senderId)
+        {
+            if (eventCode != (byte)RaiseEventCodes.UpdateObjectRoomData) { return; }
+
+            Hashtable content = (Hashtable)eventContent;
+
+            interactableObject.isKinematic = (bool)content["grabbed"];
+
+            transform.position = (Vector3)content["position"];
+            transform.rotation = (Quaternion)content["rotation"];
+
+            if (rigidBody != null)
+            {
+                rigidBody.velocity = (Vector3)content["velocity"];
+                rigidBody.angularVelocity = (Vector3)content["angularVelocity"];
+            }
+
         }
 
         private void InitState(int ownerId) {
             grabOwner = ownerId;
-            io.isGrabbable = (grabOwner == 0);
+            interactableObject.isGrabbable = (grabOwner == 0);
 
             if (ownerId == PhotonNetwork.player.ID) { return; }
 
             if (grabOwner == 0)
             {
-                io.isKinematic = wasKinimatic;
+                interactableObject.isKinematic = wasKinimatic;
             }
             else
             {
-                wasKinimatic = io.isKinematic;
-                io.isKinematic = true;
+                wasKinimatic = interactableObject.isKinematic;
+                interactableObject.isKinematic = true;
             }
             
         }
