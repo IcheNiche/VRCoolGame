685376de6fd3a7dfab99183742ff756bf17e002d
nova234 <axel_schweis2005@yahoo.de>
2018-03-05 01:31:28 +0100

    Waffen Update

---
+++ Assets/Scripts/Collision/collision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collision : MonoBehaviour
+{
+
+    public Transform AP85_Magazine;
+    void Start()
+    {
+        Transform bullet = Instantiate(AP85_Magazine) as Transform;
+        Physics.IgnoreCollision(bullet.GetComponent<Collider>(), GetComponent<Collider>());
+    }
+}
