e69ad573ef0fb34af1c12e7a42409a84e5399ea0
nova234 <axel_schweis2005@yahoo.de>
2018-03-10 11:16:37 +0100

    Update

---
+++ Assets/Scripts/Menu/Restartmap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Restartmap : MonoBehaviour {
+
+
+    public void restartmapp() {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+
+
+}
